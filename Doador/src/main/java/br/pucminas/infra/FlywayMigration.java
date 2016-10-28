ece7135c33b984a578d3d78636ca5f07bc610976
Joel Rodrigues Moreira <joel.databox@gmail.com>
2016-10-28 08:04:44 -0200

    Migração de banco de Dados

--- Doador/src/main/java/br/pucminas/infra/FlywayMigration.java
+++ Doador/src/main/java/br/pucminas/infra/FlywayMigration.java
@@ -8,10 +8,9 @@ import org.springframework.stereotype.Component;
  * Created by Joel Rodrigues on 04/10/2016.
  */
 @Component
-public class Fly implements FlywayMigrationStrategy {
+public class FlywayMigration implements FlywayMigrationStrategy {
     @Override
     public void migrate(Flyway flyway) {
-        System.out.println("chamou saporra!!!!!!!!!!!!");
         flyway.setBaselineOnMigrate(true);
         flyway.migrate();
     }
