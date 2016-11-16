80c66e652fbf1e776674d7bd4675fecf6f0d46ef
Joel Rodrigues Moreira <joel.databox@gmail.com>
2016-11-16 18:09:11 -0200

    Implementação de segurança para doadores

---
+++ Doador/src/main/java/br/pucminas/model/User.java
@@ -0,0 +1,89 @@
+package br.pucminas.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by master on 03/11/16.
+ */
+public class User implements Serializable {
+    public static final String ROLE_ADMIN = "ADM";
+    public static final String ROLE_USER = "USR";
+
+    private String userName;
+    private String passWord;
+    private String email;
+    private String description;
+    private String role;
+    private Boolean isActivated;
+    private Boolean isAdmin;
+
+    public User() {
+    }
+
+    public User(String userName, String passWord, String email, String description, String role, Boolean isActivated, Boolean isAdmin) {
+        this();
+        this.userName = userName;
+        this.passWord = passWord;
+        this.email = email;
+        this.description = description;
+        this.role = role;
+        this.isActivated = isActivated;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Boolean getActivated() {
+        return isActivated;
+    }
+
+    public void setActivated(Boolean activated) {
+        isActivated = activated;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+}
---
+++ Doador/src/main/java/br/pucminas/security/JwtFilter.java
@@ -0,0 +1,59 @@
+package br.pucminas.security;
+
+import br.pucminas.model.JwtUser;
+import br.pucminas.service.JwtService;
+import io.jsonwebtoken.JwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by master on 03/11/16.
+ */
+@WebFilter(urlPatterns = { "/api/*" })
+public class JwtFilter implements Filter {
+    @Autowired
+    private JwtService jwtTokenService;
+
+    @Value("${jwt.auth.header}")
+    String authHeader;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        final HttpServletRequest httpRequest = (HttpServletRequest) request;
+        final HttpServletResponse httpResponse = (HttpServletResponse) response;
+        final String authHeaderVal = httpRequest.getHeader(authHeader);
+        if (authHeaderVal == null || authHeaderVal.isEmpty()) {
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        try {
+            JwtUser jwtUser = jwtTokenService.getUser(authHeaderVal);
+            httpRequest.setAttribute("jwtUser", jwtUser);
+        } catch (JwtException ex) {
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        chain.doFilter(httpRequest, httpResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
