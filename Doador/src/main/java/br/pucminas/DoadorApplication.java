018b017ccf035541530d5cb66d1d6a3a6fdfb4c7
Joel Rodrigues Moreira <joel.databox@gmail.com>
2016-11-28 01:06:38 -0200

    pom.xml

--- Doador/src/main/java/br/pucminas/DoadorApplication.java
+++ Doador/src/main/java/br/pucminas/DoadorApplication.java
@@ -2,8 +2,10 @@ package br.pucminas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan(value = "br.pucminas.security")
 public class DoadorApplication {
 
     public static void main(String[] args) {
