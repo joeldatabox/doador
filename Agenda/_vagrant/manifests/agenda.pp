13c55bc4f50a8167953b53de61bd1b9cdef23380
Joel Rodrigues Moreira <joel.databox@gmail.com>
2016-11-29 18:00:35 -0200

    Script maven e de deploy da agenda-service

--- Agenda/_vagrant/manifests/agenda.pp
+++ Agenda/_vagrant/manifests/agenda.pp
@@ -3,21 +3,38 @@ node 'vagrant.puc.com' {
 }
 
 class spring::server{
-    exec {"build":
-        command => "/usr/bin/mvn package -Dmaven.test.skip=true",
-        path=> "/vagrant",
-        unless => "/usr/bin/test -e /vagrant/target/agenda-0.0.1-SNAPSHOT.jar"
-    }
-
     exec {"mkdir_agenda-service":
         command => "/bin/mkdir /opt/agenda-service",
-        unless => "/usr/bin/test -d /opt/agenda-service",
-        require => Exec["build"]
+        unless => "/usr/bin/test -d /opt/agenda-service"
     }
 
     exec {"copy":
         command => "/bin/cp /vagrant/target/agenda-service.jar /opt/agenda-service/agenda-service.jar",
         unless => "/usr/bin/test -e /opt/agenda-service/agenda-service.jar",
         require => Exec["mkdir_agenda-service"]
+    }
+
+    exec {"mkdir_agenda-service-init":
+        command => "/bin/mkdir /etc/init.d/agenda-service",
+        unless => "/usr/bin/test -d /etc/init.d/agenda-service",
+        require => Exec["mkdir_agenda-service"]
+    }
+    exec {"create_scriptInit":
+        command => "/bin/cp /vagrant/_vagrant/manifests/agenda-service-init.sh /etc/init.d/agenda-service",
+        require => Exec["mkdir_agenda-service-init"],
+        unless => ["/usr/bin/test -e /etc/init.d/agenda-service/agenda-sercice-init.sh"]
+    }
+
+    exec {"secure_boot":
+        command => "/usr/sbin/update-rc.d agenda-service/agenda-service-init.sh ",
+        require => Exec["create_scriptInit"]
+    }
+
+    service { "agenda-service":
+        ensure => running,
+        enable => true,
+        hasstatus => true,
+        hasrestart => true,
+        require => Exec["secure_boot"]
     }
 }
--- Agenda/_vagrant/manifests/maven.sh
+++ Agenda/_vagrant/manifests/maven.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get install -y maven
+
+cd /vagrant
+
+mvn package -Dmaven.test.skip=true
