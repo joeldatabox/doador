093a966471dfdf33477557ae8630aecf199e0e69
Joel Rodrigues Moreira <joel.databox@gmail.com>
2016-11-29 09:03:52 -0200

    Configuração do deploy automatizado

--- Agenda/_vagrant/manifests/agenda.pp
+++ Agenda/_vagrant/manifests/agenda.pp
@@ -5,18 +5,19 @@ node 'vagrant.puc.com' {
 class spring::server{
     exec {"build":
         command => "/usr/bin/mvn package -Dmaven.test.skip=true",
+        path=> "/vagrant",
         unless => "/usr/bin/test -e /vagrant/target/agenda-0.0.1-SNAPSHOT.jar"
     }
 
-    exec {"mkdir_agenda-service"
-        command => "/bin/mkdir /opt/agenda-service"
-        unless => "/usr/bin/test -d /opt/agenda-service"
+    exec {"mkdir_agenda-service":
+        command => "/bin/mkdir /opt/agenda-service",
+        unless => "/usr/bin/test -d /opt/agenda-service",
         require => Exec["build"]
     }
 
     exec {"copy":
-        command => "/usr/bin/mvn package -Dmaven.test.skip=true",
-        unless => "/usr/bin/test -e /vagrant/target/agenda-0.0.1-SNAPSHOT.jar"
+        command => "/bin/cp /vagrant/target/agenda-service.jar /opt/agenda-service/agenda-service.jar",
+        unless => "/usr/bin/test -e /opt/agenda-service/agenda-service.jar",
         require => Exec["mkdir_agenda-service"]
     }
 }
